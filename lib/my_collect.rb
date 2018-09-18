9eba23c5f0f23c9cada140d32cfa1d9a2ac4700d
Cheryl K <cherylkelson@icloud.com>
2018-09-18 13:15:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,14 +11,3 @@ end
   
 
   
-#   my_collect(languages) do |language|
-#     (language, languages.upcase)
-#   end 
-    
-      
-#   my_collect(students) do |student|
-#     (student, student.split(" ").first )
-#   end
-    
-# end
-
