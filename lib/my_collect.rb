2d399c95e2dd344ac7c9b5f6b89c14526fdc042f
Cheryl K <cherylkelson@icloud.com>
2018-09-18 13:15:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(languages, students)
+  i = 0 
+  collect = []
+  while i < array.length 
+    collect << yield(array[i]
+    i += 1
+  end
+  collect
+end
 
+  
+
+  
+#   my_collect(languages) do |language|
+#     (language, languages.upcase)
+#   end 
+    
+      
+#   my_collect(students) do |student|
+#     (student, student.split(" ").first )
+#   end
+    
+# end
 
